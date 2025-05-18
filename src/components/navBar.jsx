7fa38c6d83590ce902dabae5d14da197c4effd8e
Davi Mica <davimica04@gmail.com>
2025-05-18 00:12:08 -0300

    fix 3

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -5,11 +5,11 @@ function NavBar(){
     return( 
     <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/React_MovieApp">Movie App</Link>
         </div>
 
         <div className="navbar-links">
-            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/React_MovieApp" className="nav-link">Home</Link>
             <Link to="/favorites" className="nav-link">My Favorites</Link>
         </div>
     </nav>
